50825d7518815c27a6027333e602d268c21d0103
ShrishtiDubey27 <shrishtidubey5204@gmail.com>
2024-11-06 01:58:40 +0530

    basic setup for nodejs express app is done with mongoDB connection

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+  cors({
+    origin: [process.env.ORIGIN],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+const port = process.env.PORT || 3001;
+const databaseURL = process.env.DATABASE_URL;
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+mongoose
+  .connect(databaseURL)
+  .then(() => console.log("DB Connection Successful"))
+  .catch((err) => console.log("DB Connection Error:", err.message));
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send({ error: "Something went wrong!" });
+});
+const server = app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
