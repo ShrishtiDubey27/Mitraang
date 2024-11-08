88a7c0d7dfaa69775a7105621091b0e711c06dca
ShrishtiDubey27 <shrishtidubey5204@gmail.com>
2024-11-08 10:38:25 +0530

    Profile API (profile image  upload and delete feature added)

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,5 +1,5 @@
 import { useAppStore } from "@/store";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { IoArrowBack } from "react-icons/io5";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
@@ -9,7 +9,8 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { apiClient } from "@/lib/api-client";
-import { UPDATE_PROFILE_ROUTE } from "@/utils/constant";
+import { UPDATE_PROFILE_ROUTE, ADD_PROFILE_IMAGE_ROUTE, REMOVE_PROFILE_IMAGE_ROUTE } from "@/utils/constant";
+
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -19,6 +20,20 @@ const Profile = () => {
   const [image, setImage] = useState(null);
   const [hovered, setHovered] = useState(false);
   const [selectedColor, setSelectedColor] = useState(0);
+  const fileInputRef = useRef(null);
+
+  useEffect(() => {
+    if (userInfo.profileSetup) {
+      setFirstName(userInfo.firstName);
+      setLastName(userInfo.lastName);
+      setSelectedColor(userInfo.color);
+    }
+    if(userInfo.image)
+    {
+      console.log("HOST:", HOST);
+      setImage(`${HOST}/${userInfo.image}`)
+    }
+  }, [userInfo]);
 
   const validateProfile = () => {
     if (!firstName) {
@@ -51,10 +66,55 @@ const Profile = () => {
     }
   };
 
+  const handleNavigate = () => {
+    if (userInfo.profileSetup) {
+      navigate("/chat");
+    } else {
+      toast.error("Please setup profile");
+    }
+  };
+
+  const handleFileInputClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleImageChange = async (event) => {
+    const file=event.target.files[0];
+    console.log({file});
+    if(file)
+    {
+      const formData=new  FormData();
+      formData.append("profile-image",file);
+      const response = await apiClient.post(ADD_PROFILE_IMAGE_ROUTE,formData,{withCredentials:true});
+      if(response.status===200 && response.data.image)
+      {
+        setUserInfo({...userInfo,image:response.data.image});
+        toast.success("Image updated successfully");
+      }
+    
+        }
+  };
+
+  const handleDeleteImage = async () => {
+    try {
+      const response=await apiClient.delete(REMOVE_PROFILE_IMAGE_ROUTE,{
+        withCredentials:true,
+      });
+      if(response.status===200)
+      {
+        setUserInfo({...userInfo,image:null});
+        toast.success("Image removed successfully");
+        setImage(null);
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  };
+
   return (
     <div className="bg-[#1b1c24] h-[100vh] flex items-center justify-center flex-col gap-10">
       <div className="flex flex-col gap-10 w-[80vw] md:w-max">
-        <div>
+        <div onClick={handleNavigate}>
           <IoArrowBack
             className="text-4xl lg:text-6xl text-white/90 cursor-pointer"
             onClick={() => navigate(-1)}
@@ -86,7 +146,10 @@ const Profile = () => {
               )}
             </Avatar>
             {hovered && (
-              <div className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full">
+              <div
+                onClick={image ? handleDeleteImage : handleFileInputClick}
+                className="absolute inset-0 flex items-center justify-center bg-black/50 ring-fuchsia-50 rounded-full"
+              >
                 {image ? (
                   <FaTrash className="text-white text-3xl cursor-pointer" />
                 ) : (
@@ -94,6 +157,14 @@ const Profile = () => {
                 )}
               </div>
             )}
+            <input
+              type="file"
+              ref={fileInputRef}
+              className="hidden"
+              onChange={handleImageChange}
+              name="profile-image"
+              accept=".png, .jpg, .svg, .jpeg, .webp"
+            />
           </div>
           <div className="flex min-w-32 md:min-w-64 flex-col gap-5 text-white items-center justify-center">
             <div className="w-full">
